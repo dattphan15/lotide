921ddde249fbd05d129b20bd9d8419e692f514de
Kevin Phan <ken.phan@live.ca>
2021-02-09 05:06:42 +0000

    shortening index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 
+
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle
 };
